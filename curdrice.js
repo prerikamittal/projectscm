411bacbb668cee427a41193c5a32e3081594fee9
navya6116 <navya1548.becse24@chitkara.edu.in>
2025-04-24 23:45:29 +0530

    adding SIindex.html

---
+++ curdrice.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const image = document.querySelector(".recipe-image");
+    image.addEventListener("click", () => {
+      alert("Yum! Curd Rice is a refreshing treat!");
+    });
+  });
+  
